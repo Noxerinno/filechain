b2125d9ec54d054c437fef80d89b8019f690be1d
Frédéric Vaz <frederic.vaz@telecomnancy.eu>
2020-12-03 13:03:58 +0100

    Ajout du sleep dans la partie admin

--- src/filechain/admin/init.sh
+++ src/filechain/admin/init.sh
@@ -8,7 +8,7 @@ IPFS_CONT_ID=$(docker ps -aqf "name=admin_ipfs_1")
 IPFS_CLUSTER_CONT_ID=$(docker ps -aqf "name=admin_ipfs-cluster_1")
 
 echo $IPFS_CONT_ID 
-
+sleep 3
 docker exec -it $IPFS_CONT_ID ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '[\"http://0.0.0.0:5001\", \"http://localhost:3000\", \"http://127.0.0.1:5001\", \"https://webui.ipfs.io\"]'
 docker exec -it $IPFS_CONT_ID ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '[\"PUT\", \"POST\"]'
 
