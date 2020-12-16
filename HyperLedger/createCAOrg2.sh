94376499bc5b7eaa9ee481b5cd8d23c50d15bb33
Frédéric Vaz <frederic.vaz@telecomnancy.eu>
2020-12-16 23:14:26 +0100

    AJout du certificat Organisation2

---
+++ HyperLedger/createCAOrg2.sh
@@ -0,0 +1,28 @@
+export REGISTRAR_DIR=$PWD
+export FABRIC_CA_CLIENT_HOME=$REGISTRAR_DIR
+
+export ORG_DIR=$PWD/crypto-config/peerOrganizations/org2.example.com
+export PEER_TLS=$PWD/peertls
+export PEER_DIR=$ORG_DIR/peers/peer0.org2.example.com
+export REGISTRAR_DIR=$ORG_DIR/users/admin
+export ADMIN_DIR=$ORG_DIR/users/Admin@org2.example.com
+export TLS=$ORG_DIR/tlsca
+mkdir -p $ORG_DIR/ca $ORG_DIR/msp $PEER_DIR $REGISTRAR_DIR $ADMIN_DIR $TLS
+mkdir certsICA
+
+fabric-ca-client enroll -m admin -u http://adminCA:adminpw@localhost:7054 
+
+
+fabric-ca-client register --id.name ca.org2.example.com --id.type client \
+ --id.secret adminpw --csr.names C=ES,ST=Madrid,L=Madrid,O=org2.example.com \
+ --csr.cn ca.org2.example.com -m ca.org2.example.com --id.attrs  '"hf.IntermediateCA=true"' -u http://localhost:7054 
+
+
+docker-compose -f docker-compose-ca.yaml up -d ca.org2.example.com
+sudo chmod 777 -R certsICA/
+sudo chmod 777 -R certsICA/*
+sudo chgrp  -R docker $PWD/certsICA/
+sudo chown  -R 1000 $PWD/certsICA/
+cp -r $PWD/certsICA/ $ORG_DIR/ca
+
+
---
+++ HyperLedger/createCertsOrg2.sh
@@ -0,0 +1,55 @@
+# Setting environment Variables #
+export ORG_DIR=$PWD/crypto-config/peerOrganizations/org2.example.com
+export PEER_DIR=$ORG_DIR/peers/peer0.org2.example.com
+export REGISTRAR_DIR=$ORG_DIR/users/admin
+export ADMIN_DIR=$ORG_DIR/users/Admin@org2.example.com
+echo "[Step 1] Enroling client and registering peer and user identitities"
+
+export FABRIC_CA_CLIENT_HOME=$REGISTRAR_DIR
+
+# Enroll client to interact
+fabric-ca-client enroll --csr.names C=ES,ST=Madrid,L=Madrid,O=org2.example.com -m admin -u http://admin:adminpw@localhost:9054 
+
+sleep 10
+
+# Register admin identity and peer
+fabric-ca-client register --id.name Admin@org2.example.com --id.secret mysecret --id.type client --id.affiliation org2 -u http://localhost:9054 
+
+fabric-ca-client register --id.name peer0.org2.example.com --id.secret mysecret --id.type peer --id.affiliation org2 -u http://localhost:9054 
+
+
+sleep 3
+echo "[Step 1] Completed"
+
+echo "[Step 2] Creating admin certs"
+
+
+export FABRIC_CA_CLIENT_HOME=$ADMIN_DIR
+# Get certificates from ICA
+
+fabric-ca-client enroll --csr.names C=ES,ST=Madrid,L=Madrid,O=org2.example.com -m Admin@org2.example.com -u http://Admin@org2.example.com:mysecret@localhost:9054 
+mkdir -p $ADMIN_DIR/msp/admincerts && cp $ADMIN_DIR/msp/signcerts/*.pem $ADMIN_DIR/msp/admincerts/
+
+
+echo "[Step 2] Completed"
+echo "[Step 3] Creating peer certs"
+
+export FABRIC_CA_CLIENT_HOME=$PEER_DIR
+fabric-ca-client enroll --csr.names C=ES,ST=Madrid,L=Madrid,O=org2.example.com -m peer0.org2.example.com -u http://Admin@org2.example.com:mysecret@localhost:9054 
+mkdir -p $PEER_DIR/msp/admincerts && cp $ADMIN_DIR/msp/signcerts/*.pem $PEER_DIR/msp/admincerts/
+sleep 2
+echo "[Step 3] Completed"
+echo "[Step 4] Creating MSP for Organization 2"
+# Generating scaffolding
+
+mkdir -p $ORG_DIR/msp/admincerts $ORG_DIR/msp/intermediatecerts $ORG_DIR/msp/cacerts
+cp $ADMIN_DIR/msp/signcerts/*.pem $ORG_DIR/msp/admincerts/
+cp $PEER_DIR/msp/cacerts/*.pem $ORG_DIR/msp/cacerts/
+cp $PEER_DIR/msp/intermediatecerts/*.pem $ORG_DIR/msp/intermediatecerts/
+sleep 3
+echo "[Step 4] Completed"
+echo "[Step 5] Creating Scaffolfding"
+
+cp -r $PWD/certsICA/* $ORG_DIR/ca
+echo "[Step 5] Completed"
+
