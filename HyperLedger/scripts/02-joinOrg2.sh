2d29b6712c50d73fffa87e49b869c747f52188b8
Frédéric Vaz <frederic.vaz@telecomnancy.eu>
2020-12-16 23:53:41 +0100

    AJout de Org2 au channel

--- HyperLedger/scripts/02-joinOrg2.sh
+++ HyperLedger/scripts/02-joinOrg2.sh
@@ -1,8 +1,8 @@
 
 ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/myapp.com/orderers/orderer0.myapp.com/msp/tlscacerts/tlsca.myapp.com.crt.pem
-CORE_PEER_LOCALMSPID="Org1MSP"
-CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-CORE_PEER_ADDRESS=${IP_PEER_ORG1}:7051
+CORE_PEER_LOCALMSPID="Org2MSP"
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+CORE_PEER_ADDRESS=${IP_PEER_ORG2}:8051
 CHANNEL_NAME=channel1
 CORE_PEER_TLS_ENABLED=false
 ORDERER_SYSCHAN_ID=syschain
