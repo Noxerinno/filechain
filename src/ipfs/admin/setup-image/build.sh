84bb3f31301a654324a2a19312c98296dfcba67e
gseghaier <gilles.seghaier@gmail.com>
2021-01-04 11:07:31 +0100

    Remove S3 env variables

--- src/ipfs/admin/setup-image/build.sh
+++ src/ipfs/admin/setup-image/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-AWS_ACCESS_KEY_ID=AKIAWUFYU45RSPJGHIZK # required unless IAM_ROLE is set
-AWS_SECRET_ACCESS_KEY=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 # required unless IAM_ROLE is set
+AWS_ACCESS_KEY_ID= # required unless IAM_ROLE is set
+AWS_SECRET_ACCESS_KEY= # required unless IAM_ROLE is set
 S3_ACL=private # default, optional
-S3_BUCKET=heroku-polls-user # required
+S3_BUCKET= # required
 IAM_ROLE= # optional IAM role name, for usage on EC2.
 
 docker build -t setup-docker-build .
