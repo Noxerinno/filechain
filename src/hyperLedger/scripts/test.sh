78f327babed02079dd580ec0b675185982b3f751
Frédéric Vaz <frederic.vaz@telecomnancy.eu>
2021-01-07 13:21:47 +0100

    Correction some errors due to script.sh

--- src/hyperLedger/scripts/test.sh
+++ src/hyperLedger/scripts/test.sh
@@ -26,12 +26,12 @@ CHANNEL_NAME=channel1
 CORE_PEER_TLS_ENABLED=false
 ORDERER_SYSCHAN_ID=syschain
 
-peer lifecycle chaincode package simple-contract.tar.gz --path github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 --lang golang --label simple-contract_1.0
-peer lifecycle chaincode install simple-contract.tar.gz
+peer lifecycle chaincode package chaincode_example02_1.tar.gz --path github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 --lang golang --label chaincode_example02_1.0
+peer lifecycle chaincode install chaincode_example02_1.tar.gz
 peer lifecycle chaincode queryinstalled
-export PACKAGE_ID=simple-contract_1.0:36937a32d6d11383b705b6ab14eb9fa069c77a30d93ebf1d6f743c2690d0ab62
-#export PACKAGE_ID=chaincode_example02_1.0:da6b8e40e6680181a0017df9d4a7afacc05c8cc41e802d9896235c1b2c7545a9
-peer lifecycle chaincode approveformyorg -o orderer0.org1.example.com:7050 --cafile $ORDERER_CA --channelID channel1 --name "simple-contract" --version 1.0 --init-required --package-id $PACKAGE_ID --sequence 1 --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
+#export PACKAGE_ID=simple-contract_1.0:36937a32d6d11383b705b6ab14eb9fa069c77a30d93ebf1d6f743c2690d0ab62
+export PACKAGE_ID=chaincode_example02_1.0:da6b8e40e6680181a0017df9d4a7afacc05c8cc41e802d9896235c1b2c7545a9
+peer lifecycle chaincode approveformyorg -o orderer0.org1.example.com:7050 --cafile $ORDERER_CA --channelID channel1 --name "chaincode_example02_1" --version 1.0 --init-required --package-id $PACKAGE_ID --sequence 1 --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
 
 #Installing in peer Org2
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
@@ -39,19 +39,20 @@ export CORE_PEER_ADDRESS=${IP_PEER_ORG2}:8051
 export CORE_PEER_LOCALMSPID="Org2MSP"
 
 
-peer lifecycle chaincode package simple-contract.tar.gz --path github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 --lang golang --label simple-contract_1.0
-peer lifecycle chaincode install simple-contract.tar.gz
+peer lifecycle chaincode package chaincode_example02_1.tar.gz --path github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 --lang golang --label chaincode_example02_1.0
+peer lifecycle chaincode install chaincode_example02_1.tar.gz
 peer lifecycle chaincode queryinstalled
-export PACKAGE_ID=simple-contract_1.0:68ed2fcbb549b38c0a0550886dd7d139d16902313b0c5e20d05c41ec555f70e7
+#export PACKAGE_ID=simple-contract_1.0:36937a32d6d11383b705b6ab14eb9fa069c77a30d93ebf1d6f743c2690d0ab62
+export PACKAGE_ID=chaincode_example02_1.0:da6b8e40e6680181a0017df9d4a7afacc05c8cc41e802d9896235c1b2c7545a9
 
-peer lifecycle chaincode approveformyorg -o orderer0.org1.example.com:7050 --cafile $ORDERER_CA --channelID channel1 --name "simple-contract" --version 1.0 --init-required --package-id $PACKAGE_ID --sequence 1 --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
+peer lifecycle chaincode approveformyorg -o orderer0.org1.example.com:7050 --cafile $ORDERER_CA --channelID channel1 --name "chaincode_example02_1" --version 1.0 --init-required --package-id $PACKAGE_ID --sequence 1 --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
 
-peer lifecycle chaincode checkcommitreadiness --channelID channel1 --name "simple-contract" --version 1.0 --cafile $ORDERER_CA --output json --sequence 1 --init-required --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
+peer lifecycle chaincode checkcommitreadiness --channelID channel1 --name "chaincode_example02_1" --version 1.0 --cafile $ORDERER_CA --output json --sequence 1 --init-required --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
 
-peer lifecycle chaincode commit -o orderer0.org1.example.com:7050 --channelID channel1 --name "simple-contract" --version 1.0 --sequence 1 --init-required --cafile $ORDERER_CA --peerAddresses ${IP_PEER_ORG1}:7051 --peerAddresses ${IP_PEER_ORG2}:8051 --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
-#peer lifecycle chaincode querycommitted --channelID channel1 --name "simple-contract" --cafile $ORDERER_CA
-#peer chaincode invoke -n "simple-contract" -c '{"Args":["Create", "KEY_1", "VALUE_1"]}' -C channel1
-peer chaincode invoke -n "simple-contract" -c '{"Args":["Create", "KEY_1", "VALUE_1"]}' -C channel1 -o orderer0.org1.example.com:7050 --cafile $ORDERER_CA --peerAddresses ${IP_PEER_ORG1}:7051 --peerAddresses ${IP_PEER_ORG2}:8051 --isInit
+peer lifecycle chaincode commit -o orderer0.org1.example.com:7050 --channelID channel1 --name "chaincode_example02_1" --version 1.0 --sequence 1 --init-required --cafile $ORDERER_CA --peerAddresses ${IP_PEER_ORG1}:7051 --peerAddresses ${IP_PEER_ORG2}:8051 --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
+#peer lifecycle chaincode querycommitted --channelID channel1 --name "chaincode_example02_1" --cafile $ORDERER_CA
+#peer chaincode invoke -n "chaincode_example02_1" -c '{"Args":["Create", "KEY_1", "VALUE_1"]}' -C channel1
+peer chaincode invoke -n "chaincode_example02_1" -c '{"Args":["Create", "KEY_1", "VALUE_1"]}' -C channel1 -o orderer0.org1.example.com:7050 --cafile $ORDERER_CA --peerAddresses ${IP_PEER_ORG1}:7051 --peerAddresses ${IP_PEER_ORG2}:8051 --isInit
 
 #peer chaincode install -n mycontract2 -v 0 -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 >&log.txt
 
