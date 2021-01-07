7f552b518d216f7179bcb6f90f853cf7f7a2f46b
Frédéric Vaz <frederic.vaz@telecomnancy.eu>
2021-01-07 14:53:32 +0100

    Change in test.sh to find the package id automatically

--- src/hyperLedger/scripts/test.sh
+++ src/hyperLedger/scripts/test.sh
@@ -29,8 +29,10 @@ ORDERER_SYSCHAN_ID=syschain
 peer lifecycle chaincode package chaincode_example02_1.tar.gz --path github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 --lang golang --label chaincode_example02_1.0
 peer lifecycle chaincode install chaincode_example02_1.tar.gz
 peer lifecycle chaincode queryinstalled
+touch test.txt
+peer lifecycle chaincode queryinstalled > text.txt
 #export PACKAGE_ID=simple-contract_1.0:36937a32d6d11383b705b6ab14eb9fa069c77a30d93ebf1d6f743c2690d0ab62
-export PACKAGE_ID=chaincode_example02_1.0:da6b8e40e6680181a0017df9d4a7afacc05c8cc41e802d9896235c1b2c7545a9
+export PACKAGE_ID=$(cat text.txt | grep  "Package ID" | cut -d' ' -f3 | sed 's/.$//')
 peer lifecycle chaincode approveformyorg -o orderer0.org1.example.com:7050 --cafile $ORDERER_CA --channelID channel1 --name "chaincode_example02_1" --version 1.0 --init-required --package-id $PACKAGE_ID --sequence 1 --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
 
 #Installing in peer Org2
@@ -41,9 +43,10 @@ export CORE_PEER_LOCALMSPID="Org2MSP"
 
 peer lifecycle chaincode package chaincode_example02_1.tar.gz --path github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 --lang golang --label chaincode_example02_1.0
 peer lifecycle chaincode install chaincode_example02_1.tar.gz
-peer lifecycle chaincode queryinstalled
+touch test.txt
+peer lifecycle chaincode queryinstalled > text.txt
 #export PACKAGE_ID=simple-contract_1.0:36937a32d6d11383b705b6ab14eb9fa069c77a30d93ebf1d6f743c2690d0ab62
-export PACKAGE_ID=chaincode_example02_1.0:da6b8e40e6680181a0017df9d4a7afacc05c8cc41e802d9896235c1b2c7545a9
+export PACKAGE_ID=$(cat text.txt | grep  "Package ID" | cut -d' ' -f3 | sed 's/.$//')
 
 peer lifecycle chaincode approveformyorg -o orderer0.org1.example.com:7050 --cafile $ORDERER_CA --channelID channel1 --name "chaincode_example02_1" --version 1.0 --init-required --package-id $PACKAGE_ID --sequence 1 --signature-policy "OR('Org1MSP.peer','Org2MSP.peer')"
 
