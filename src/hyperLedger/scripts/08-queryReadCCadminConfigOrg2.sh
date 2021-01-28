75c3bf46ad600cbc0c611df045c882769112dd9e
Frédéric Vaz <frederic.vaz@telecomnancy.eu>
2021-01-28 14:38:35 +0100

    Adding scripts for Read & Delete of the chaincode

--- src/hyperLedger/scripts/08-queryReadCCadminConfigOrg2.sh
+++ src/hyperLedger/scripts/08-queryReadCCadminConfigOrg2.sh
@@ -14,6 +14,8 @@
 
 # ==============================================================================
 
+# 1 argument is required: Key
+
 ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/filechain/crypto-config/peerOrganizations/org1.example.com/orderers/orderer0.org1.example.com/msp/tlscacerts/tlsca.org1.example.com.crt.pem
 CORE_PEER_LOCALMSPID="Org2MSP"
 # CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/filechain/crypto-config/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com.crt.pem
@@ -23,5 +25,10 @@ CHANNEL_NAME=channel1
 CORE_PEER_TLS_ENABLED=false
 ORDERER_SYSCHAN_ID=syschain
 
-#read -p "Press any key to continue (query ReadAll) ..."
-peer chaincode query -n adminConfig-contract -c '{"Args":["ReadAll"]}' -C $CHANNEL_NAME #2>/dev/null
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters. 1 argument required."
+    exit 1
+fi
+
+#read -p "Press any key to continue (query Read) ..."
+peer chaincode query -n adminConfig-contract -c '{"Args":["Read","'$1'"]}' -C $CHANNEL_NAME #2>/dev/null
